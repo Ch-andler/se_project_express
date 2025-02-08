d655a7db020deee26c07963626f67f662f10553d
Ch-andler <chandlerpedini@gmail.com>
2025-02-08 09:40:21 -0500

    fixed issues

--- controllers/users.js
+++ controllers/users.js
@@ -85,7 +85,7 @@ const login = (req, res) => {
     .catch((err) => {
       if (err.message === "Incorrect email or password") {
         res
-          .status(unauthorized)
+          .status(serverError)
           .send({ message: "An error has occurred on the server" });
       }
     });
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,12 @@
 const router = require("express").Router();
-const {
-  getUsers,
-  updateCurrentUser,
-  getCurrentUser,
-} = require("../controllers/users");
+const { updateCurrentUser, getCurrentUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
 // Protect all user routes with the auth middleware
 router.use(auth);
 
 // Get the current user's details (protected route)
-router.get("/me", getUsers, getCurrentUser);
+router.get("/me", getCurrentUser);
 
 // Update the current user's details (protected route)
 router.patch("/me", updateCurrentUser);
